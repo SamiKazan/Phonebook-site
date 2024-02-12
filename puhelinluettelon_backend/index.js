06dcf0c9409366749ad1d058caa92b802a4f198e
SamiKazan <sami.kazan@helsinki.fi>
2024-02-12 15:25:51 +0200

    tehtävän 3.9 lisäykset

--- puhelinluettelon_backend/index.js
+++ puhelinluettelon_backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
+
+
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -86,6 +95,11 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+
+
+
+
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
